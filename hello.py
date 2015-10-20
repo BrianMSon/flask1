059299a03d27c59f34c4bed0001e750ed37a4a06
BrianMSon <sonmg00@gmail.com>
2015-10-20 15:27:04 +0900

    modify hello.py

--- hello.py
+++ hello.py
@@ -1,8 +1,14 @@
-import os
 from flask import Flask
-
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-    return 'Hello World!'
+def hello_world():
+    return 'Hello World!!!'
+
+@app.route('/hello')
+def show_hello():
+    return 'Show Hello!'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
+	
